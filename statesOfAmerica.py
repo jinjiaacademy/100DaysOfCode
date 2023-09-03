d383b8f3a9ff4a0b99ffc5766af08b1b159b4be0
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-03 11:20:29 -0400

    Create statesOfAmerica.py

---
+++ statesOfAmerica.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  3 11:13:51 2023
+
+@author: Jinjia
+"""
+
+statesOfAmerica = ['Delaware', 'Pennsylvania', 'New Jersey', 
+                  'Georgia', 'Connecticut', 'Massachusetts', 
+                  'Maryland', 'South Carolina', 'New Hampshire',
+                  'Virginia', 'New York', 'North Carolina', 
+                  'Rhode Island', 'Vermont', 'Kentucky', 
+                  'Tennessee', 'Ohio', 'Louisiana', 'Indiana', 
+                  'Mississippi', 'Illinois', 'Alabama', 'Maine',
+                  'Missouri', 'Arkansas', 'Michigan', 'Florida',
+                  'Texas', 'Iowa', 'Wisconsin', 'California', 
+                  'Minnesota', 'Oregon', 'Kansas', 'West Virginia', 
+                  'Nevada', 'Nebraska', 'Colorado', 'North Dakota', 
+                  'South Dakota', 'Montana', 'Washington', 
+                  'Idaho', 'Wyoming', 'Utah', 'Oklahoma', 
+                  'New Mexico', 'Arizona', 'Alaska', 'Hawaii']
+
+print(statesOfAmerica[-4])
