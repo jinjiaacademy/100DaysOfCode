40206a06208155ba5dbdaffb672970e57270236c
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-02 23:22:24 -0400

    Create day1Printing.py

---
+++ day1Printing.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  1 09:37:54 2023
+
+@author: jinjialiu
+"""
+
+print("Hello world!")
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Using \n to print the words in a new line
+print("Hello world!\nHello world!\nHello world!")
+
+# Using string concatenation
+print("Hello" + " " + "Jinjia")
+
+# input function
+print("What is your name? ")
+name = input()
+print("Hello " + name)
+
+# Enter your name and return the length of your name
+name = input("What is your name? ")
+print(len(name))
+
+# in spyder, to comment out a line of code use command + 1
