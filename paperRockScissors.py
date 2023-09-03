5161a7dd4f2d2045b576bd4429b464481ba3b960
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-03 14:21:04 -0400

    Update paperRockScissors.py

--- paperRockScissors.py
+++ paperRockScissors.py
@@ -15,29 +15,28 @@ if human not in choices:
     print("You entered the invalid words, Game over!!")
 else:
     print(f"You: {human} vs. Computer: {computer}")
-
-if computer == "paper":
-    if human == "paper":
-        print("Tie")
-    else:
-        if human == "rock":
-            print("Computer wins")
-        else:
-            print("Human wins")
-else:
-    if computer == "rock":
+    if computer == "paper":
         if human == "paper":
-            print("Human wins")
+            print("Tie")
         else:
             if human == "rock":
-                print("Tie")
-            else:
                 print("Computer wins")
+            else:
+                print("Human wins")
     else:
-        if human == "paper":
-            print("Computer wins")
-        else:
-            if human == "rock":
+        if computer == "rock":
+            if human == "paper":
                 print("Human wins")
             else:
-                print("Tie")
+                if human == "rock":
+                    print("Tie")
+                else:
+                    print("Computer wins")
+        else:
+            if human == "paper":
+                print("Computer wins")
+            else:
+                if human == "rock":
+                    print("Human wins")
+                else:
+                    print("Tie")
