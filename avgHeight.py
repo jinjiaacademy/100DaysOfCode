c7a6b95fd5d10092fe3827c6a66307add4162af8
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-03 14:20:57 -0400

    Create avgHeight.py

---
+++ avgHeight.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  3 14:08:48 2023
+
+@author: Jinjia Liu
+"""
+
+# You are going to write a program that calculates the average student
+# height from a list of heights.
+
+total = 0
+students = int(input("How many students: "))
+
+for i in range(1, students+1):
+    height = int(input(f"Enter the No.{i} student's height in cm: "))
+    total += height    
+averageHeight = total/students
+
+print(f"There are {students} students in the group and the average height is {averageHeight:.2f}cm.")
