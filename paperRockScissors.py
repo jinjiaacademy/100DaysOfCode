8a29da6c125c367df139a1b713facba94bbc1969
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-03 13:05:36 -0400

    Create paperRockScissors.py

---
+++ paperRockScissors.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  3 11:56:10 2023
+
+@author: Jinjia Liu
+"""
+
+import random
+
+human = input("What do you choose? Paper, rock or scissor? ").lower()
+computer = random.choice(['paper', 'rock', 'scissor'])
+print(f"You: {human} vs. Computer: {computer}")
+
+if computer == "paper":
+    if human == "paper":
+        print("Tie")
+    else:
+        if human == "rock":
+            print("Computer wins")
+        else:
+            print("Human wins")
+else:
+    if computer == "rock":
+        if human == "paper":
+            print("Human wins")
+        else:
+            if human == "rock":
+                print("Tie")
+            else:
+                print("Computer wins")
+    else:
+        if human == "paper":
+            print("Computer wins")
+        else:
+            if human == "rock":
+                print("Human wins")
+            else:
+                print("Tie")
