e1f47acc0c672408c3a75a0d99304f95f9aca5e6
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-03 21:17:33 -0400

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -2,6 +2,17 @@
 """
 Created on Sun Sep  3 14:49:31 2023
 
-@author: Jinjia
+@author: Jinjia Liu
 """
 
+for number in range(1, 101):
+    if number % 3 == 0:
+        if number % 5 == 0:
+            print('FizzBuzz')
+        else:
+            print('Fizz')
+    else:
+        if number % 5 == 0:
+            print('Buzz')
+        else:
+            print(number)
