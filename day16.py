6dc80933eaddc1233d58454ddaaa7af2ffc29f48
jinjiaacademy <63980125+jinjiaacademy@users.noreply.github.com>
2023-09-22 10:54:20 -0400

    Create day16.py

---
+++ day16.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 13:57:54 2023
+
+@author: Jinjia
+"""
+
+import turtle
+
+timmy = turtle.Turtle()
+my_screen = turtle.Screen()
+
+my_screen.exitonclick()
